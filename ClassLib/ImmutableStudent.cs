1d23dc79a051804db91bd24da0e9b7226d38fbab
freyjabun <freyjabun@gmail.com>
2021-09-23 15:13:40 +0200

    i hate it here

--- ClassLib/ImmutableStudent.cs
+++ ClassLib/ImmutableStudent.cs
@@ -1,7 +1,7 @@
 using System;
 
 namespace ClassLib {
-    public record ImmutableStudent(int id, string givenName, string surname, DateTime startDate, DateTime endDate, DateTime graduationDate)
+    public record ImmutableStudent()
 {
     int id{get; init;}
     string givenName{get; init;}
@@ -10,5 +10,20 @@ namespace ClassLib {
     DateTime startDate{get; init;}
     DateTime endDate{get; init;}
     DateTime graduationDate{get; init;}
+
+    // public ImmutableStudent(int id, string givenName, string surname, DateTime startDate, DateTime endDate, DateTime graduationDate)
+    //     {
+    //         this.id = id;
+    //         this.givenName = givenName;
+    //         this.surname = surname;
+    //         this.startDate = startDate;
+    //         this.endDate = endDate;
+    //         this.graduationDate = graduationDate;
+
+    //         if(startDate.Equals(DateTime.Now)) {status = status.New;}
+    //         else if (endDate == default) {status = status.Active;}
+    //         else if (endDate < graduationDate) {status = status.Dropout;}
+    //         else status = status.Graduate;
+    //     }
 }
 }
