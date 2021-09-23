b368ebe2324c0482ec2c852837d44f2ff1a66203
freyjabun <freyjabun@gmail.com>
2021-09-23 16:42:54 +0200

    Fixed indentation

--- ClassLib/ImmutableStudent.cs
+++ ClassLib/ImmutableStudent.cs
@@ -4,18 +4,18 @@ namespace ClassLib
 {
     public record ImmutableStudent(int id, string givenName, string surname, DateTime startDate, DateTime endDate, DateTime graduationDate)
     {
-    public status status
-    {
-        get => GetStatus();
-    }
+        public status status
+        {
+            get => GetStatus();
+        }
 
-    private status GetStatus()
-    {
+        private status GetStatus()
+        {
         var now = DateTime.Now;
         if (now < startDate.AddMonths(3) && now < endDate) return status.New;
         if (startDate <  now && now < endDate) return status.Active;
         if (endDate == graduationDate) return status.Graduate;
         return status.Dropout;
-    }
+        }
     }
 }
